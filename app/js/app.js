b3916844d4ad4aea2eb45f73428ada8c1b91c99e
yourUsername <your_email@example.com>
2017-01-25 10:59:57 -0500

    updated rest

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,19 @@
   .module('empireCo', [
     'ui.router'
   ]);
+empreCo.config(function($stateProvider, $urlRouterProvider){
+  $urlProvider.otherwise('/home');
 
+
+  $stateProvider
+
+  .state('home',{
+      url: '/home',
+      templateUrl: 'index.html',
+      controller: function($scope){
+        $scope.products [gear]
+      }
+  })
+})
 
 })();
