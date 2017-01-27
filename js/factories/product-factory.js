ebf1d36a12c474c59cc88196a340f5b5fe89b6c9
yourUsername <your_email@example.com>
2017-01-27 12:46:08 -0500

    changed resource url for production environment

--- js/factories/product-factory.js
+++ js/factories/product-factory.js
@@ -2,7 +2,11 @@
     angular
     .module('empireCo')
     .factory('ProductFactory', function ($resource) {
+<<<<<<< HEAD
     return $resource("http://localhost:8080/api/products");
+=======
+    return $resource("https://empire-co.herokuapp.com/api/products");    
+>>>>>>> changed resource url for production environment
 });
 
 })();
